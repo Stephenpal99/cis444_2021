0756bd291d8cab32879c5f715022c44a13a1f3c5
Stephenpal99 <90297870+Stephenpal99@users.noreply.github.com>
2021-11-20 19:44:59 -0800

    Update app.py

--- flask_jwt_rest_server/app.py
+++ flask_jwt_rest_server/app.py
@@ -1,88 +1,135 @@
-from flask import Flask,render_template,request, redirect, url_for, g
+from flask import Flask,render_template,request, jsonify, redirect
 from flask_json import FlaskJSON, JsonError, json_response, as_json
 import jwt
 
-import sys
 import datetime
 import bcrypt
-import traceback
 
 
 from db_con import get_db_instance, get_db
 
-from tools.token_required import token_required
-from tools.get_aws_secrets import get_secrets
 
 from tools.logging import logger
 
 ERROR_MSG = "Ooops.. Didn't work!"
 
-
-#Create our app
 app = Flask(__name__)
-#add in flask json
 FlaskJSON(app)
 
-#g is flask for a global var storage 
-def init_new_env():
-    if 'db' not in g:
-        g.db = get_db()
+JWT_SECRET = None
+CUR_ENV = "PRD"
 
-    g.secrets = get_secrets()
+global_db_con = get_db()
 
-#This gets executed by default by the browser if no page is specified
-#So.. we redirect to the endpoint we want to load the base page
+token = None
+
 @app.route('/') #endpoint
 def index():
-    return redirect('/static/first_form.html')
+    return redirect('/static/index.html')
 
 
-@app.route("/secure_api/<proc_name>",methods=['GET', 'POST'])
-@token_required
-def exec_secure_proc(proc_name):
-    logger.debug(f"Secure Call to {proc_name}")
+@app.route('/backp',  methods=['POST']) #endpoint
+def backp():
+    print(request.form)
+    salted = bcrypt.hashpw( bytes(request.form['fname'],  'utf-8' ) , bcrypt.gensalt(10))
+    print(salted)
 
-    #setup the env
-    init_new_env()
+    print(  bcrypt.checkpw(  bytes(request.form['fname'],  'utf-8' )  , salted ))
 
-    #see if we can execute it..
-    resp = ""
-    try:
-        fn = getattr(__import__('secure_calls.'+proc_name), proc_name)
-        resp = fn.handle_request()
-    except Exception as err:
-        ex_data = str(Exception) + '\n'
-        ex_data = ex_data + str(err) + '\n'
-        ex_data = ex_data + traceback.format_exc()
-        logger.error(ex_data)
-        return json_response(status_=500 ,data=ERROR_MSG)
+    return render_template('backatu.html',input_from_browser= str(request.form) )
 
-    return resp
+
+@app.route('/exposejwt') #endpoint
+def exposejwt():
+    jwt_token = request.args.get('jwt')
+    print(jwt_token)
+    return json_response(output=jwt.decode(jwt_token, JWT_SECRET, algorithms=["HS256"]))
 
 
 
-@app.route("/open_api/<proc_name>",methods=['GET', 'POST'])
-def exec_proc(proc_name):
-    logger.debug(f"Call to {proc_name}")
+app.config['SECRET_KEY'] = 'helloworld'
 
-    #setup the env
-    init_new_env()
+def create_token(user):
+    payload = list(user)
+    token = jwt.encode({'username': user, 'exp': datetime.datetime.utcnow() + datetime.timedelta(minutes=30)}, app.config['SECRET_KEY'])
+    return token
 
-    #see if we can execute it..
-    resp = ""
-    try:
-        fn = getattr(__import__('open_calls.'+proc_name), proc_name)
-        resp = fn.handle_request()
-    except Exception as err:
-        ex_data = str(Exception) + '\n'
-        ex_data = ex_data + str(err) + '\n'
-        ex_data = ex_data + traceback.format_exc()
-        logger.error(ex_data)
-        return json_response(status_=500 ,data=ERROR_MSG)
+@app.route('/addUser',methods = ["GET","POST"])
+def addUser():
+	logger.debug(f"Add user")
+	if request.method == "POST":
+		cur =global_db_con.cursor()
+		user = request.form.get("username")
+		password = request.form.get("password")
+		enc_password = bcrypt.hashpw(bytes(password,"utf-8"),bcrypt.gensalt())
+		userInsert = """INSERT INTO users(username,pass) values(%s, %s);"""
+		cur.execute(userInsert,(user,enc_password))
+		global_db_con.commit()
+		print("Your user account has successfully been created. Please login now.")
+		return "Welcome" + user
+	logger.error(user)
+	return json_response(status_=500 ,data=ERROR_MSG)
 
-    return resp
+@app.route('/getUser', methods=['POST'])
+def login():
+    user = request.form['username']
+    password = request.form['password']
+    cur = global_db_con.cursor()
+    cur.execute(f"SELECT pass FROM users WHERE username = '{user}';")
+    checkr = cur.fetchone()[0]
+    if checkr == None:
+        print("The username was not found")
+        return "User not found"
+    elif(checkr == password):
+        global token
+        token = create_token(user)
+        return redirect('/static/myprofile.html')
+    else:
+        return "Invalid password"
+        
+      
+      	
+@app.route('/getMyBooks', methods = ["GET", "POST"])
+def myBooks():
+    cur = global_db_con.cursor() 
+    global token
+    getUser = jwt.decode(token, app.config['SECRET_KEY'], algorithms=["HS256"])
+    username = getUser['username']
+    #username = "stevep"
+    sqlExecute = (f"SELECT book_title FROM owners WHERE username = '{username}';")
+    cur.execute(sqlExecute)
+    rows = cur.fetchall()
+    if rows == None:
+        return "You don't own any books"
+    else:
+        return jsonify(str(rows))
 
+@app.route('/buyBook_id_321', methods = ["GET", "POST"])
+def buyCatHat():
+    cur = global_db_con.cursor() 
+    global token
+    getUser = jwt.decode(token, app.config['SECRET_KEY'], algorithms=["HS256"])
+    username = getUser['username']
+    book_title = "Cat With Hat"
+    #username = "stevep"
+    sqlExecute = """INSERT INTO owners(username,book_title) VALUES(%s,%s);"""
+    cur.execute(sqlExecute,(username,book_title))
+    global_db_con.commit()
+    return username + "has successfully purchased the book Cat With Hat"
+   
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=80)
+@app.route('/buyBook_id_123', methods = ["GET", "POST"])
+def buyMocking():
+    cur = global_db_con.cursor() 
+    global token
+    getUser = jwt.decode(token, app.config['SECRET_KEY'], algorithms=["HS256"])
+    username = getUser['username']
+    book_title = "Kill Mockingbird"
+    #username = "stevep"
+    sqlExecute = """INSERT INTO owners(username,book_title) VALUES(%s,%s);"""
+    cur.execute(sqlExecute,(username,book_title))
+    global_db_con.commit()
+    return username + " has successfully purchased the book Kill Mockingbird"
+      
 
+app.run(host='0.0.0.0', port=80)
